868b12836628fa26fd00a5068a3b211081536e3d
FiniteZ <johnconham@cox.net>
2018-05-07 01:24:41 -0400

    added database connection to DeckAdapter, and cards

--- app/src/main/java/edu/rosehulman/fenogljc/mtgbazaar/adapters/DeckAdapter.java
+++ app/src/main/java/edu/rosehulman/fenogljc/mtgbazaar/adapters/DeckAdapter.java
@@ -1,15 +1,24 @@
 package edu.rosehulman.fenogljc.mtgbazaar.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+
+import edu.rosehulman.fenogljc.mtgbazaar.Deck;
 import edu.rosehulman.fenogljc.mtgbazaar.R;
 import edu.rosehulman.fenogljc.mtgbazaar.Card;
 import edu.rosehulman.fenogljc.mtgbazaar.fragments.DeckFragment.OnListFragmentInteractionListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,10 +30,15 @@ public class DeckAdapter extends RecyclerView.Adapter<DeckAdapter.ViewHolder> {
 
     private final List<Card> mValues;
     private final OnListFragmentInteractionListener mListener;
+    private final Context mContext;
+    private DatabaseReference mDeckRef;
 
-    public DeckAdapter(List<Card> items, OnListFragmentInteractionListener listener) {
-        mValues = items;
+    public DeckAdapter(Context context, OnListFragmentInteractionListener listener, DatabaseReference databaseReference) {
+        mContext = context;
+        mValues = new ArrayList<>();
         mListener = listener;
+        mDeckRef = databaseReference;
+        mDeckRef.addChildEventListener(new CardChildEventListener());
     }
 
     @Override
@@ -72,6 +86,51 @@ public class DeckAdapter extends RecyclerView.Adapter<DeckAdapter.ViewHolder> {
         @Override
         public String toString() {
             return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+
+    private class CardChildEventListener implements ChildEventListener {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+            Card card = dataSnapshot.getValue(Card.class);
+            card.setKey(dataSnapshot.getKey());
+            mValues.add(0, card);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            String key = dataSnapshot.getKey();
+            Card updatedCard = dataSnapshot.getValue(Card.class);
+            for (Card c : mValues) {
+                if (c.getKey().equals(key)) {
+                    c.setValues(updatedCard);
+                    break;
+                }
+            }
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+            String key = dataSnapshot.getKey();
+            for (Card c : mValues) {
+                if (c.getKey().equals(key)) {
+                    mValues.remove(c);
+                    break;
+                }
+            }
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            Log.e("mtgBazaar", databaseError.getMessage());
         }
     }
 }
