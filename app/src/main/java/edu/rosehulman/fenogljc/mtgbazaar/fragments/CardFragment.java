4c7ed5e90c4f2a495481fcb899ce0deb66fa30ff
FiniteZ <johnconham@cox.net>
2018-05-14 18:13:36 -0400

    Card search now loads dummy card

--- app/src/main/java/edu/rosehulman/fenogljc/mtgbazaar/fragments/CardFragment.java
+++ app/src/main/java/edu/rosehulman/fenogljc/mtgbazaar/fragments/CardFragment.java
@@ -2,6 +2,9 @@ package edu.rosehulman.fenogljc.mtgbazaar.fragments;
 
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
@@ -13,6 +16,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -21,6 +25,17 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 import java.util.Locale;
 
 import edu.rosehulman.fenogljc.mtgbazaar.Callback;
@@ -35,6 +50,8 @@ import edu.rosehulman.fenogljc.mtgbazaar.models.UserCard;
  */
 public class CardFragment extends Fragment {
 
+
+    private ImageView mImageView;
 
     public CardFragment() {
         // Required empty public constructor
@@ -54,6 +71,7 @@ public class CardFragment extends Fragment {
         layoutView.findViewById(R.id.pricing_layout).setVisibility(View.INVISIBLE);
         layoutView.findViewById(R.id.card_info_layout).setVisibility(View.INVISIBLE);
         layoutView.findViewById(R.id.card_text).setVisibility(View.INVISIBLE);
+        layoutView.findViewById(R.id.card_image).setVisibility(View.INVISIBLE);
 
         final AutoCompleteTextView autoComplete = layoutView.findViewById(R.id.card_search);
 
@@ -100,11 +118,15 @@ public class CardFragment extends Fragment {
         Spinner setSelector = layoutView.findViewById(R.id.pricing_set);
         TextView regPrice = layoutView.findViewById(R.id.regular_price);
         TextView foilPrice = layoutView.findViewById(R.id.foil_price);
+        mImageView = layoutView.findViewById(R.id.card_image);
 
         cardName.setText(incomingCard.getKey());
         cardType.setText(incomingCard.getType().toString() + " - " + incomingCard.getSubtype().toString());
         cardManaCost.setText(incomingCard.getManaCost());
         cardText.setText(incomingCard.getText());
+        
+        //TODO: get image from multiverse ID
+        (new GetImageClass()).execute("https://api.scryfall.com/cards/multiverse/" + incomingCard.getMultiverseId());
 
         //TODO: FILL SELECTOR AND PRICES
         ArrayAdapter<String> setAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, incomingCard.getSets());
@@ -116,5 +138,54 @@ public class CardFragment extends Fragment {
         layoutView.findViewById(R.id.pricing_layout).setVisibility(View.VISIBLE);
         layoutView.findViewById(R.id.card_info_layout).setVisibility(View.VISIBLE);
         layoutView.findViewById(R.id.card_text).setVisibility(View.VISIBLE);
+        layoutView.findViewById(R.id.card_image).setVisibility(View.VISIBLE);
+    }
+
+    class GetImageClass extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urlStrings) {
+
+            String urlString = urlStrings[0];
+            Log.d("PICS", "doInBackground: " + urlString);
+            URL url;
+            Bitmap bmp = null;
+            try {
+                // Fetch the card info from scryfall
+                url = new URL(urlString);
+                InputStream is = url.openStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+                String jsonText = readAll(rd);
+                JSONObject json = new JSONObject(jsonText);
+
+                // Get the url for the image and download that image
+                String imageURL = json.getJSONObject("image_uris").getString("normal");
+                url = new URL(imageURL);
+                bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return bmp;
+        }
+
+        private String readAll(BufferedReader rd) throws IOException {
+            StringBuilder sb = new StringBuilder();
+            int cp;
+            while ((cp = rd.read()) != -1) {
+                sb.append((char) cp);
+            }
+            return sb.toString();
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            mImageView.setImageBitmap(bitmap);
+        }
     }
 }
