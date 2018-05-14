3f47b03a5a7334543a50ab367520e61165aadb4e
FiniteZ <johnconham@cox.net>
2018-05-13 20:11:30 -0400

    Added things to enable card to be deserialized

--- app/src/main/java/edu/rosehulman/fenogljc/mtgbazaar/Callback.java
+++ app/src/main/java/edu/rosehulman/fenogljc/mtgbazaar/Callback.java
@@ -1,5 +1,8 @@
 package edu.rosehulman.fenogljc.mtgbazaar;
 
+import edu.rosehulman.fenogljc.mtgbazaar.models.UserCard;
+
 public interface Callback {
     void onEdit();
+    void onCardFound(UserCard card);
 }
